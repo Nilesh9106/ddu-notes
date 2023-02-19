b95bd011d5f9776918e4534c084573333fa31c95
Nilesh darji <nilesh.28.2003@gmail.com>
2023-02-19 12:53:35 +0530

    notes

--- noteshare/settings.py
+++ noteshare/settings.py
@@ -150,8 +150,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'darjin412@gmail.com' 
-EMAIL_HOST_PASSWORD = 'xsvd inxf arip ufad'
+EMAIL_HOST_USER = '' 
+EMAIL_HOST_PASSWORD = ''
 
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
